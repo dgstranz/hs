319e75ce9fb3c5d0c6cd6ce6c58c717b7d03b46d
dgstranz <dgstranz@gmail.com>
2016-07-11 20:48:50 +0200

    more form logic

--- app/components/card/cardController.js
+++ app/components/card/cardController.js
@@ -8,7 +8,7 @@ app.controller('cardController', ['$scope', 'cardFactory', function($scope, card
 		console.log('Error loading cards.');
 	});
 	
-	$scope.showCards = true; // Should be false in production
+	$scope.showCards = false; // Should be false in production
 	
 	$scope.query = {};
 
@@ -22,4 +22,72 @@ app.controller('cardController', ['$scope', 'cardFactory', function($scope, card
 	};
 
 	$scope.reset();
+	
+	$scope.numberFields = ['cost', 'attack', 'health', 'durability'];
+	
+	$scope.cardFilter = function(card) {
+		var query = $scope.query;
+		var numberFields = ['cost', 'attack', 'health', 'durability'];
+		var standardSets = ['CORE', 'EXPERT1', 'TGT', 'OG', 'BRM', 'LOE'];
+		var output = true;
+		
+		Object.byString = function(o, s) {
+			s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+			s = s.replace(/^\./, '');           // strip a leading dot
+			var a = s.split('.');
+			for (var i = 0, n = a.length; i < n; ++i) {
+				var k = a[i];
+				if (k in o) {
+					o = o[k];
+				} else {
+					return;
+				}
+			}
+			return o;
+		}
+		
+		var exactValueFilter = function(card, queryAttr, cardAttr) {
+			cardAttr = cardAttr || queryAttr;
+			if(/^\d+$/.test(Object.byString(query, queryAttr))) {
+				return Object.byString(card, cardAttr) == Object.byString(query, queryAttr);
+			} else return true;
+		};
+		var minFilter = function(card, queryAttr, cardAttr) {
+			cardAttr = cardAttr || queryAttr;
+			if(/^\d+$/.test(Object.byString(query, queryAttr))) {
+				return Object.byString(card, cardAttr) >= Object.byString(query, queryAttr);
+			} else return true;
+		};
+		var maxFilter = function(card, queryAttr, cardAttr) {
+			cardAttr = cardAttr || queryAttr;
+			if(/^\d+$/.test(Object.byString(query, queryAttr))) {
+				return Object.byString(card, cardAttr) <= Object.byString(query, queryAttr);
+			} else return true;
+		};
+		var rangeFilter = function(card, attribute) {
+			return minFilter(card, attribute + '.min', attribute) && maxFilter(card, attribute + '.max', attribute);
+		};
+		
+		var numberFilter = function(card, attribute) {
+			if(!query[attribute]) {
+				return true;
+			} else if(query[attribute].mode == 'exact') {
+				return exactValueFilter(card, attribute + '.value', attribute);
+			} else if(query[attribute].mode == 'range') {
+				return rangeFilter(card, attribute);
+			}
+			return true;
+		};
+		
+		//numberFields.forEach(function(field) {
+		for (var i = 0; i < numberFields.length; i++) {
+			if (!numberFilter(card, numberFields[i])) {
+				return false;
+			}
+		};
+		
+		//return rangeFilter(card, 'cost');
+		//return (card.set == query.set);
+		return true;
+	}
 }]);
